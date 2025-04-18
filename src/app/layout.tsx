f608c3bd0c2f3a5e3e390285ab3faee35ccf920d
Elemental-Games <mmdiorio3@gmail.com>
2025-04-18 14:44:45 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} bg-black text-white`}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
